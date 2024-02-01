eb8631cc3c96efb0cdf8d8027e92e4d77d45bb70
Steven Tang <steven.tang.kh@gmail.com>
2024-02-01 21:31:45 +0800

    [Tutorial] Exercise 4.3

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -3,3 +3,18 @@ from odoo import fields, models
 class Property(models.Model):
     _name = "estate.property"
     _description = "Properties of an estate"
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [("North", "north"),("West", "west"),("East","east"),("South","south")]
+    )
