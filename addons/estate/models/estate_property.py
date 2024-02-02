5e6b9e12769ce5492d93c9efd933c3a1c1e35e28
Steven Tang <steven.tang.kh@gmail.com>
2024-02-02 11:37:56 +0800

    [Tutorial] Exercise 7.1
    Getting Started of Odoo's Tutorial

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -3,14 +3,14 @@ from odoo import fields, models
 class Property(models.Model):
     _name = "estate.property"
     _description = "Properties of an estate"
-    name = fields.Char(required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    date_availability = fields.Date(string="Available From", copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
